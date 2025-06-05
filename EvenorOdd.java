ba3561e344988580f1db08b12462b4b0aca03379
ABDULAFRIDEE <shaikafridee@gmail.com>
2025-06-05 16:19:16 +0530

    Add files via upload

---
+++ EvenorOdd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class EvenorOdd {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+       
+        System.out.print("Enter number: ");
+        int ut = scanner.nextInt();
+        if(ut%2==0) {
+        	System.out.println("even");
+        	
+        }else {
+        	System.out.println("odd");
+        }
+
+		
+
+	}
+
+}
---
+++ StudentMR.java
@@ -0,0 +1,8 @@
+
+public class StudentMR {
+	public static void main(String[]args) {
+		
+		
+	}
+
+}
